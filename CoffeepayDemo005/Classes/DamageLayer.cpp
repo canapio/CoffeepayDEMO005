dba26b5d86415e506cd958ffd642ab269ddc36da
seungmini <semicpu@gmail.com>
2015-08-04 22:08:38 +0900

    DamageLayer.cpp 만 커밋
    error: open("CoffeepayDemo005/proj.win32/CoffeepayDemo005.opensdf"): Permission denied
    error: unable to index file CoffeepayDemo005/proj.win32/CoffeepayDemo005.opensdf

--- CoffeepayDemo005/Classes/DamageLayer.cpp
+++ CoffeepayDemo005/Classes/DamageLayer.cpp
@@ -20,33 +20,25 @@ void DamageLayer::show_damage(int damage, Enemy* input_en, int delay, Layer* ly)
 	Size sprite_size = input_en->get_sprite()->getContentSize();
 	Point sprite_point = input_en->getPosition();
 	label_damage->setColor(Color3B::RED);
-	//label_damage->setPosition(Point(sprite_point.x , sprite_point.y + sprite_size.height * SCALE_RATE / 2.0f + SCALEUP_VALUE(5) ));
-	label_damage->setPosition(sprite_point.x, 0);
-	//input_en->get_sprite()->setOpacity(155);
+	label_damage->setPosition(Point(sprite_point.x , sprite_point.y + sprite_size.height * SCALE_RATE / 2.0f + SCALEUP_VALUE(5) ));
+	
 	
 	
 	ly->addChild(label_damage, z_order_enemy_health);
 	
 	float time = delay * 0.1f;
-<<<<<<< HEAD
 	auto action_damage_move = MoveBy::create(time, Point(0, SCALEUP_VALUE(10)));
 	auto action_damage_fadeout = FadeOut::create(time);
 	//auto action_blink = Blink::create(time/5, 1);
 	auto action_spawn = Spawn::create(action_damage_move, action_damage_fadeout,NULL);
-	auto action_tint_1 = FadeTo::create(0.01, 120);
+	auto action_tint_120 = FadeTo::create(0.01, 120);
 	auto action_delay = DelayTime::create(time / 10);
-	auto action_tint_2 = FadeTo::create(0.01, 255);
-	auto action_spawn_fade = Sequence::create(action_tint_1,action_delay, action_tint_2, NULL);
-	label_damage->runAction(action_spawn);
-	//input_en->get_sprite()->runAction(action_tint_1);
+	auto action_tint_255 = FadeTo::create(0.01, 255);
+	auto action_spawn_fade = Sequence::create(action_tint_120,action_delay, action_tint_255, NULL);
+	
 	input_en->get_sprite()->runAction(action_spawn_fade);
-=======
-	auto action_damage_move = MoveBy::create(1.0f, Point(0, SCALEUP_VALUE(10)));
-	auto action_damage_fadeout = FadeOut::create(1.0f);
-
-	auto action_spawn = Spawn::create(action_damage_move, action_damage_fadeout,NULL);
+	
 	
 	label_damage->runAction(Sequence::create(action_spawn,RemoveSelf::create(),NULL));
->>>>>>> ca3dfe820f12f46babab802dfeddc7a1587ab340
 
 }
